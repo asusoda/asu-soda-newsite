b1adca8a531e6bbcb3dfea688ead0b71c0ad7992
Ben Juntilla <ben@benjuntilla.com>
2024-06-09 16:37:33 -0700

    position navbar and footer at extremities

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col justify-between">
       <NavigationBar />
       <Home />
       <Footer />
-    </>
+    </div>
   );
 }
 
