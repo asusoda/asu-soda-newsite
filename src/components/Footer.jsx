d4bfa4160ed1c6755c6fb476bba4a1c2586bdc91
Ben Juntilla <ben@benjuntilla.com>
2024-06-09 16:58:35 -0700

    add social media links to hero

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className=" w-full border-t-1">
       <hr className="border-gray-500 opacity-100"></hr>
-      <div className="grid grid-cols-4 max-md:grid-cols-2 gap-10 px-20 py-10 max-md:gap-5 max-md:px-10 max-md:py-5">
+      <div className="grid grid-cols-3 max-md:grid-cols-2 gap-10 px-20 py-10 max-md:gap-5 max-md:px-10 max-md:py-5">
         <div className="text-slate-300 max-md:col-span-2 max-md:p-5">
           SoDA - Software Developer Association at ASU
         </div>
@@ -31,54 +31,6 @@ function Footer() {
           <a href="#">
             <p className="text-gray-500">Privacy & Policy</p>
           </a>
-        </div>
-
-        <div className="max-md:col-span-2 max-md:justify-self-center">
-          <a href="#" className="max-md:hidden">
-            <p className="text-slate-300">Contacts</p>
-          </a>
-          <div className="flex gap-5 py-5">
-            <a href="#">
-              <img
-                src="/discord-white-logo.png"
-                width={30}
-                height={30}
-                alt="logo"
-              ></img>
-            </a>
-            <a href="#">
-              <img
-                src="/github-mark-white.png"
-                width={30}
-                height={30}
-                alt="logo"
-              ></img>
-            </a>
-            <a href="#">
-              <img
-                src="/instagram-white-logo.png"
-                width={30}
-                height={30}
-                alt="logo"
-              ></img>
-            </a>
-            <a href="#">
-              <img
-                src="/linkedin-white-logo.png"
-                width={30}
-                height={30}
-                alt="logo"
-              ></img>
-            </a>
-            <a href="#">
-              <img
-                src="/link-white-icon.png"
-                width={30}
-                height={30}
-                alt="logo"
-              ></img>
-            </a>
-          </div>
         </div>
       </div>
       <div className=" text-center items-center bg-[#121212] text-gray-400 py-5">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,74 @@ function Hero() {
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">./SoDA</h1>
           <p class="my-2">The Software Developers Association</p>
-          <p class="my-4">
+          <p class="mt-4">
             The premiere software development club for university students at Arizona State University
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <ul className="menu menu-horizontal bg-base-100 rounded-box mt-6">
+            <li>
+              <a className="tooltip" data-tip="Sun Devil Sync" href="https://asu.campuslabs.com/engage/organization/soda">
+              <img
+                src="/link-white-icon.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="Discord" href="https://discord.com/invite/6mpAPKk">
+                <img
+                  src="/discord-white-logo.png"
+                  className="h-6 w-6"
+                  alt="logo"
+                />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="Instagram" href="https://www.instagram.com/soda.asu/">
+              <img
+                src="/instagram-white-logo.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="X" href="https://x.com/asu_soda">
+              <img
+                src="/link-white-icon.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="Facebook" href="https://www.facebook.com/SoDAASU/">
+              <img
+                src="/link-white-icon.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="LinkedIn" href="https://www.linkedin.com/company/thesoda/">
+              <img
+                src="/linkedin-white-logo.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+            <li>
+              <a className="tooltip" data-tip="Github" href="https://github.com/asusoda">
+              <img
+                src="/github-mark-white.png"
+                className="h-6 w-6"
+                alt="logo"
+              />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
