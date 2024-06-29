09a43369072a5e4e31dcc5b0d1a55e864aab6323
Avery-Powers <aapcalculus@gmail.com>
2024-06-29 11:34:11 -0700

    include team card and info card components

---
+++ src/components/Info/InfoList.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const content =
+	[
+		{
+			"title": "How To Join SoDA",
+			"content": [
+				<p>Want to join SoDA? Just go to one of our meetings! Everyone who attends our meetings is a SoDA member. </p>,
+			],
+            "button": "Join our SunDevilSync"
+		},
+		{
+			"title": "Professional Development",
+			"content": [
+				<p>We want students to become familiar with industry by helping them prepare for technical interviews, and showing them how and when to apply.</p>,
+			],
+            "button": "Learn more"
+		},
+		{
+			"title": "Technical Workshops",
+			"content": [
+				<p>Want to join SoDA? Just go to one of our meetings! Everyone who attends our meetings is a SoDA member.</p>,
+			],
+            "button": "Learn more"
+		},
+		{
+			"title": "SoDA Code Challenge X",
+			"content": [
+				<p>Want to join SoDA? Just go to one of our meetings! Everyone who attends our meetings is a SoDA member.</p>,
+			],
+            "button": "Sign up now"
+		},
+		// {
+		// 	"title": "Hackathon",
+		// 	"content": [
+		// 		<p>A hackathon is a design sprint-like event in which computer programmers and others involved in software development, including graphic designers, interface designers, project managers, and others, often including subject-matter-experts, collaborate intensively on software projects.</p>
+		// 	]
+		// },
+		{
+			"title": "Community",
+			"content": [
+				<p>Want to join SoDA? Just go to one of our meetings! Everyone who attends our meetings is a SoDA member.</p>
+			],
+            "button": "Join our SunDevilSync"
+		}
+	];
+
+export default content;
---
+++ src/components/Team/TeamCards.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { Card, Divider, Popup, Image} from 'semantic-ui-react';
+
+import contacts from './TeamList.json';
+import './TeamCards.css';
+
+class MemberCards extends Component{
+
+	constructor(props) {
+		super(props);
+		this.onGClick = this.onGClick.bind(this);
+		this.onIClick = this.onIClick.bind(this);
+	}
+	onGClick() {
+		window.location.href = 'mailto:asu@thesoda.io';
+	}
+	onIClick() {
+		window.location.href = 'mailto:industry.asu@thesoda.io';
+	}
+
+	render() {
+		const teams = contacts.teams
+        const advisors = contacts.advisors
+
+		return (
+			<div className="team">
+				<Divider horizontal id="title">Team</Divider><br/>
+				<div id="content">
+					SoDA is here to help you. Please feel free to reach out to any of us, we want to help and would be happy to answer any questions you have!
+				</div>
+
+				{Object.keys(teams).map((team, i) =>
+					<React.Fragment key={i}>
+						<Divider horizontal className="subteam-divider">{team}</Divider>
+						<div id="cards">
+							{teams[team].map((member, j) => (
+								<Card key={j} className="Card rounded-card">
+								<Card.Content className="card-content">
+
+									<div className="card-image-wrapper">
+										<Image className="card-image" src={member.image} size="tiny" />
+									</div>
+									
+									<div className="card-text">
+										<Card.Header className="card-header">{member.name}</Card.Header>
+										<Card.Meta className="card-meta">
+										{member.role}
+										<br />
+										<Popup 
+											key={member.name}
+											position='bottom center'
+											inverted
+											trigger={<a href={'mailto:' + member.email} className="card-email">{member.email}</a>}
+											//header='Open Mail Client'
+											//content={member.email}
+										/>
+										</Card.Meta>
+									</div>
+
+								</Card.Content>
+								</Card>
+							))}
+						</div>
+					</React.Fragment>
+				)}
+
+                <Divider horizontal className="subteam-divider">Advisors</Divider>
+                <div id="cards">
+                    {advisors.map((advisor, j) => (
+                        <Card key={j} className="Card rounded-card">
+							<Card.Content className="card-content">
+						
+								<div className="card-image-wrapper">
+									<Image className="card-image" src={advisor.image} size="tiny" />
+								</div>
+
+								<div className="card-text">
+									<Card.Header className="card-header">{advisor.name}</Card.Header>
+									<Card.Meta className="card-meta">
+										{advisor.role}
+										<br />
+										<a href={'mailto:' + advisor.email} className="card-email">{advisor.email}</a>
+									</Card.Meta>
+								</div>
+
+							</Card.Content>
+                        </Card>
+                    ))}
+                </div>
+
+			</div>
+		);
+	}
+}
+
+export default MemberCards;
