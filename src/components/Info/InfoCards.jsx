945b3f7e236bf1fbaf8fb99a3c60dd51bc2653bb
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-06-30 12:29:22 -0700

    Add changes to Navbar

--- src/components/Info/InfoCards.jsx
+++ src/components/Info/InfoCards.jsx
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
-import content from './InfoList';
-import {Card, Button, Divider} from 'semantic-ui-react';
-import './InfoCards.css'
+import { Component } from "react";
+import content from "./InfoList";
+import { Card, Button, Divider } from "semantic-ui-react";
+import "./InfoCards.css";
 
 class InfoCards extends Component {
-    render() {
-		return (
-			<div className="infocards">
-                <Divider horizontal id="title">Info</Divider><br/>
+  render() {
+    return (
+      <div className="infocards" id="info">
+        <Divider horizontal id="title">
+          Info
+        </Divider>
+        <br />
 
-                <div id="cards">
-                    {content.map(({title, content, button}) =>
-                        <Card key={title} className="Card rounded-card">
-                            <Card.Content className="card-content">
-                                <Card.Header className="card-header">{title}</Card.Header>
-                                <Card.Description className="card-description">{content}</Card.Description>
-                                <br/>
-                                <Button
-                                    className="card-button"
-                                    
-                                > 
-                                {button} </Button>
-                            </Card.Content>
-                        </Card>
-                    )}
-                </div>
-			</div>
-		);
-	}
+        <div id="cards">
+          {content.map(({ title, content, button }) => (
+            <Card key={title} className="Card rounded-card">
+              <Card.Content className="card-content">
+                <Card.Header className="card-header">{title}</Card.Header>
+                <Card.Description className="card-description">
+                  {content}
+                </Card.Description>
+                <br />
+                <Button className="card-button">{button} </Button>
+              </Card.Content>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default InfoCards;
-
-
-
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,18 +10,31 @@ function Navbar() {
     <nav className="w-full bg-black-dark fixed">
       {/* Primary Navigation - Hidden on mobile */}
       <section className="w-full justify-between hidden sm:flex py-5 px-10">
-        <a href="/">
-          <h1 className="text-purple-dark text-[21px] text-soda-red font-extrabold">
+        <a href="/" className="flex gap-3 items-center">
+          <img src="/logo/soda-logo-red.png" className="w-8"></img>
+          <h1 className="text-purple-dark text-[21px] text-soda-white font-extrabold text-3xl">
             SoDA
           </h1>
         </a>
 
-        <div className="flex gap-10">
-          <h3>Info</h3>
-          <h3>Sponsors</h3>
-          <h3>Team</h3>
-        </div>
-        <Button name="Contact" />
+        <ul className="flex gap-10 items-center text-soda-white">
+          <li>
+            <a href="#info" className="nav-text-animation">
+              Info
+            </a>
+          </li>
+          <li>
+            <a href="#sponsors" className="nav-text-animation">
+              Sponsors
+            </a>
+          </li>
+          <li>
+            <a href="#team" className="nav-text-animation">
+              Team
+            </a>
+          </li>
+        </ul>
+        <Button name="Join SoDA" />
       </section>
 
       {/*Mobile Navigation*/}
