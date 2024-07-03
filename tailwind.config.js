02d0f4ea0ceebb28faf344cf4fc2427e206e9394
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-03 10:43:54 -0700

    Added Animated Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,17 +82,17 @@ export default {
       },
     },
     animation: {
-      marquee: 'marquee 25s linear infinite',
-      marquee2: 'marquee2 25s linear infinite',
+      marquee: "marquee 25s linear infinite",
+      marquee2: "marquee2 25s linear infinite",
     },
     keyframes: {
       marquee: {
-        '0%': { transform: 'translateX(0%)' },
-        '100%': { transform: 'translateX(-100%)' },
+        "0%": { transform: "translateX(0%)" },
+        "100%": { transform: "translateX(-100%)" },
       },
       marquee2: {
-        '0%': { transform: 'translateX(100%)' },
-        '100%': { transform: 'translateX(0%)' },
+        "0%": { transform: "translateX(100%)" },
+        "100%": { transform: "translateX(0%)" },
       },
     },
     fontFamily: {
