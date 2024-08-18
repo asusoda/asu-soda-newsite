eded3115af805ef8d51fbc61f75e50f8df79bcfc
Ben Juntilla <ben@benjuntilla.com>
2024-08-18 13:51:32 -0700

    add image optimizer to build step and install `@types/prop-types`

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer()
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
