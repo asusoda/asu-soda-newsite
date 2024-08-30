e8179de89af637721ba824bf84fd1b6e72eb8d58
Avery-Powers <102402101+Swimming7birdz@users.noreply.github.com>
2024-08-30 13:36:32 -0700

    merge main (#20)

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <nav className="navigation">
+    <nav className="navigation" style={{ marginTop: 0 }}>
       {/* Primary Navigation - Hidden on mobile */}
       <DesktopNav />
       {/*Mobile Navigation*/}
