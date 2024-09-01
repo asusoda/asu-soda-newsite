f32e7a13df39f6af61f32d209836f4a11a872e4d
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-09-01 12:05:50 -0700

    Fixed SoDA Logo with auto margin

--- src/components/EventsPhotoCarousel.tsx
+++ src/components/EventsPhotoCarousel.tsx
@@ -2,7 +2,7 @@ import placeholder from "../assets/logo-placeholder.png";
 
 function EventsPhotoCarousel() {
   return (
-    <div className="max-md:hidden aspect-square">
+    <div className="max-md:hidden aspect-square mx-auto">
       <img src={placeholder} width={400} alt="place holder" />
     </div>
   );
