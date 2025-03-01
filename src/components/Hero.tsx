e724bfbf93dbcd6e0229e934f70f27d576f0f4e8
Ben Juntilla <ben@benjuntilla.com>
2025-03-01 01:49:34 -0700

    get rid of duplicate text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,16 +10,9 @@ function Hero() {
       <main className="hero">
         <section className="hero-container">
           <summary className="left-hero">
-            <h1 className="hero-large-text">
+            <h1 className="hero-large-text mb-10">
               The Software Developers Association
             </h1>
-            <p className="hero-small-text max-md:hidden">
-              SoDA is one of the largest communities of students at Arizona
-              State University. We provide opportunities to collaborate on
-              projects outside of the classroom, learn from university and
-              industry professionals, and prepare for a career as a software
-              developer.
-            </p>
             <Statistics />
             <SocialMediaLinks />
           </summary>
