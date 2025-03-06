b337889bea19e664eeb43f0bcaad2b413bc0d3ec
Ben Juntilla <ben@benjuntilla.com>
2025-03-06 02:56:16 -0700

    no goofy blue cards

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -33,7 +33,7 @@ const IndividualBlog: React.FC<IndividualBlogProps> = ({
 }) => {
   return (
     <Link to={link} className="group relative block">
-      <div className="bg-gray-900 border-gray-600  rounded-lg p-6 max-w-md h-full overflow-hidden text-white font-sans transition transform hover:scale-105 hover:shadow-lg">
+      <div className="bg-neutral-900 border-gray-600  rounded-lg p-6 max-w-md h-full overflow-hidden text-white font-sans transition transform hover:scale-105 hover:shadow-lg">
         <img src={imageURL} alt={alt} className="w-full rounded-lg" />
         <div className="flex flex-wrap gap-2 mt-2">
           {tag.map((t, index) => (
