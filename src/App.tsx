37aa03a2977c79b8f44d2e3694ebb13f93a780a9
Ben Juntilla <ben@benjuntilla.com>
2025-03-31 09:13:16 -0700

    import heavier weight

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "@fontsource/space-grotesk";
+import "@fontsource/space-grotesk/700.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
 import NavbarSection from "./components/Navigation/Navbar";
